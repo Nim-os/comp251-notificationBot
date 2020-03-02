a6661117c304e5d608f26cd24bb37b03ad4b16cd
Nim-os <10841304+Nim-os@users.noreply.github.com>
2020-03-01 22:01:48 -0500

    Finish most of bot script

--- botScript.sh
+++ botScript.sh
@@ -2,8 +2,25 @@
 
 echo "Starting bot..."
 
-while true;
+ut=0
+
+while true
 do
-    # Get page as txt file
+    # Update uptime
+    ut+=1
+
+    # Get page as txt file and trim page
     wget https://www.luc.devroye.org/251.html -0 raw.txt
+
+    cat raw.txt | grep '' > trim.txt
+
+    # Run Python checker
+    python3 checker.py
+
+    rm *.txt
+
+    # Waiting between wget calls helps prevent requests from being blocked
+    sleep 60
 done
+
+echo -e "Bot finished. Up time: $ut cycles"
