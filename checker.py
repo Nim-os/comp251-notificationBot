3b00f289a15d6e3ebf66ab28711b83f12032fffb
Nim-os <10841304+Nim-os@users.noreply.github.com>
2020-03-02 20:30:44 -0500

    Fix most issues. Change break point.

--- checker.py
+++ checker.py
@@ -1,26 +1,30 @@
 from last import lastMsg
 
-stop = "<font class=\"bold\"><BR>"
-nxt = False
+stop = "New / Messages&nbsp;&nbsp;</font><BR></TD>" # Old break: "<font class=\"bold\"><BR>"
+nxt = 0
 msg = ""
 
-raw = open("trim.txt", "r")
+raw = open("raw.txt", "r")
 
 # Checks for the breaking point within the html
 for line in raw:
     if stop in line:
-        nxt = True
-    if nxt
+        nxt += 1
+        continue
+    if nxt != 0:
+        nxt += 1
+    if nxt == 4:
         msg = line
         break
 
-# Removes initial 3 characters (<P>)
-msg = msg[3:]
+# Removes <P> new line character
+msg = msg[3:-1]
 
 if (msg == lastMsg):
-    print "No new messages"
+    print("No new messages")
 else:
-    print "New message from luc.devroye.org/251.html: " + msg
-    last = open("last.py","r+")
+    print("New message from luc.devroye.org/251.html\n" + msg)
+    last = open("last.py","w")
     last.seek(0)
-    last.write("lastMsg = " + msg)
+    last.write("lastMsg = \"" + msg + "\"")
+    #last.write("\"") # Needed because weird behaviour when trying to write " after msg
